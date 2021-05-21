697e8e0a33f8a39f94afdbf4e87763f42e899120
SaminYeaser <saminyeaser1@gmail.com>
2021-05-21 06:23:48 +0600

    chart logic made

---
+++ lib/widget/chart.dart
@@ -0,0 +1,39 @@
+import 'package:expense_manager/model/transaction.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class Chart extends StatelessWidget {
+  const Chart({Key key, this.recentTransaction}) : super(key: key);
+  final List<Transaction> recentTransaction;
+
+  List<Map<String, Object>> get groupedTransaction {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(Duration(days: index));
+
+      double totalSum = 0.0;
+      for(var i;i<recentTransaction.length;i++){
+        if(recentTransaction[i].date.day ==  weekDay.day &&
+            recentTransaction[i].date.month ==  weekDay.month &&
+            recentTransaction[i].date.year ==  weekDay.year){
+              totalSum = totalSum + recentTransaction[i].price;
+        }
+      }
+      return {'day': DateFormat.E(weekDay), 'amount': totalSum};
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+      child: Row(
+        children: [
+          Column(
+            children: [],
+          )
+        ],
+      ),
+    );
+  }
+}
