11203e80084e3107efc06b4fec2e5a339ddccfec
SaminYeaser <saminyeaser1@gmail.com>
2021-05-22 20:32:00 +0600

    chart modified

--- lib/widget/chart_bar.dart
+++ lib/widget/chart_bar.dart
@@ -11,7 +11,9 @@ class ChartBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        FittedBox(child: Text('৳${spending.toStringAsFixed(0)}')),
+        Container(
+          height: 20,
+            child: FittedBox(child: Text('৳${spending.toStringAsFixed(0)}'))),
         SizedBox(
           height: 4,
         ),
@@ -24,15 +26,15 @@ class ChartBar extends StatelessWidget {
                 decoration: BoxDecoration(
                     border: Border.all(color: Colors.grey, width: 1.0),
                     color: Color.fromRGBO(220, 220, 220, 1),
-                    borderRadius: BorderRadius.circular(10)
-                ),
+                    borderRadius: BorderRadius.circular(10)),
               ),
               FractionallySizedBox(
                 heightFactor: spendingPercentageTotal,
-                child: Container(decoration: BoxDecoration(
-                  color: Theme.of(context).primaryColor,
-                    borderRadius: BorderRadius.circular(10)
-                ),),
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.circular(10)),
+                ),
               ),
             ],
           ),
